58e37e227f921f1cc749f50a1079ce0b229cf09e
Ashuh <ashuh223@gmail.com>
2022-03-06 03:28:29 +0800

    Simplify Disqus post retrieval logic

--- app/src/main/java/com/ashuh/nusmoduleplanner/api/disqus/DisqusApiInterface.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/api/disqus/DisqusApiInterface.java
@@ -1,7 +1,6 @@
 package com.ashuh.nusmoduleplanner.api.disqus;
 
 import com.ashuh.nusmoduleplanner.data.disqus.PostList;
-import com.ashuh.nusmoduleplanner.data.disqus.ThreadDetails;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,11 +8,7 @@ import retrofit2.http.Query;
 
 public interface DisqusApiInterface {
 
-    @GET("posts/list.json")
-    Call<PostList> getPosts(@Query("api_key") String key, @Query("forum") String forum, @Query(
-            "thread") long thread);
-
-    @GET("threads/details.json")
-    Call<ThreadDetails> getThreadDetails(@Query("api_key") String apiKey, @Query("forum") String forum,
-                                         @Query("thread:ident") String moduleCode);
+    @GET("threads/listPosts.json")
+    Call<PostList> getPosts(@Query("api_key") String apiKey, @Query("forum") String forum,
+                            @Query("thread:ident") String moduleCode);
 }
--- app/src/main/java/com/ashuh/nusmoduleplanner/data/DisqusRepository.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/data/DisqusRepository.java
@@ -12,7 +12,6 @@ import com.ashuh.nusmoduleplanner.BuildConfig;
 import com.ashuh.nusmoduleplanner.api.disqus.DisqusApiInterface;
 import com.ashuh.nusmoduleplanner.api.disqus.DisqusApiInterfaceBuilder;
 import com.ashuh.nusmoduleplanner.data.disqus.PostList;
-import com.ashuh.nusmoduleplanner.data.disqus.ThreadDetails;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,47 +24,23 @@ public class DisqusRepository {
     private static final String TAG = "DisqusRepository";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static LiveData<ThreadDetails> getThreadDetails(String moduleCode) {
-        MutableLiveData<ThreadDetails> threadDetailsLiveData = new MutableLiveData<>();
+    public static LiveData<PostList> getPosts(String moduleCode) {
+        MutableLiveData<PostList> liveData = new MutableLiveData<>();
         DisqusApiInterface api = DisqusApiInterfaceBuilder.getApiInterface();
-        Call<ThreadDetails> call = api.getThreadDetails(KEY, FORUM, moduleCode);
-        call.enqueue(new Callback<ThreadDetails>() {
+        Call<PostList> call = api.getPosts(KEY, FORUM, moduleCode);
+        call.enqueue(new Callback<PostList>() {
             @Override
-            public void onResponse(@NonNull Call<ThreadDetails> call,
-                                   @NonNull Response<ThreadDetails> response) {
-                threadDetailsLiveData.setValue(response.body());
+            public void onResponse(@NonNull Call<PostList> call,
+                                   @NonNull Response<PostList> response) {
+                liveData.setValue(response.body());
             }
 
             @Override
-            public void onFailure(@NonNull Call<ThreadDetails> call, @NonNull Throwable t) {
+            public void onFailure(@NonNull Call<PostList> call, @NonNull Throwable t) {
                 Log.d(TAG, "onFailure: " + t);
             }
         });
 
-        return threadDetailsLiveData;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public static LiveData<PostList> getPosts(String moduleCode) {
-        MutableLiveData<PostList> postListLiveData = new MutableLiveData<>();
-        getThreadDetails(moduleCode).observeForever(threadDetails -> {
-            DisqusApiInterface api = DisqusApiInterfaceBuilder.getApiInterface();
-            Call<PostList> call = api.getPosts(KEY, FORUM, threadDetails.getThread().getId());
-
-            call.enqueue(new Callback<PostList>() {
-                @Override
-                public void onResponse(@NonNull Call<PostList> call,
-                                       @NonNull Response<PostList> response) {
-                    postListLiveData.setValue(response.body());
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<PostList> call, @NonNull Throwable t) {
-                    Log.d(TAG, "onFailure: " + t);
-                }
-            });
-        });
-
-        return postListLiveData;
+        return liveData;
     }
 }
