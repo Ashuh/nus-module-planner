4575e3f86a7ac6e9e5f1f5d3560c272076a7ff45
Ashuh <ashuh223@gmail.com>
2022-03-07 00:41:42 +0800

    Add more methods in ModuleDetailFragment and reorganize code

--- app/src/main/java/com/ashuh/nusmoduleplanner/ui/moduledetail/ModuleDetailFragment.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/ui/moduledetail/ModuleDetailFragment.java
@@ -25,6 +25,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ashuh.nusmoduleplanner.MainActivity;
 import com.ashuh.nusmoduleplanner.R;
+import com.ashuh.nusmoduleplanner.data.model.nusmods.ModuleDetail;
 import com.ashuh.nusmoduleplanner.data.model.nusmods.Semester;
 import com.ashuh.nusmoduleplanner.data.model.nusmods.SemesterDetail;
 import com.ashuh.nusmoduleplanner.data.model.timetable.AssignedModule;
@@ -33,25 +34,81 @@ import com.ashuh.nusmoduleplanner.data.source.ModulesRepository;
 import com.ashuh.nusmoduleplanner.data.source.TimetableDataSource;
 import com.ashuh.nusmoduleplanner.util.AcademicYear;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class ModuleDetailFragment extends Fragment {
-    private ModuleDetailViewModel viewModel;
+    private TextView titleTextView;
+    private TextView codeTextView;
+    private TextView infoTextView;
+    private TextView semestersTextView;
+    private TextView descriptionTextView;
+    private TextView prerequisiteTextView;
+    private TextView corequisiteTextView;
+    private TextView preclusionTextView;
+    private TextView prerequisiteHeaderTextView;
+    private TextView corequisiteHeaderTextView;
+    private TextView preclusionHeaderTextView;
+    private TextView sem3ExamTextView;
+    private TextView sem1ExamTextView;
+    private TextView sem2ExamTextView;
+    private TextView sem4ExamTextView;
+    private TextView sem1ExamHeadingTextView;
+    private TextView sem2ExamHeadingTextView;
+    private TextView sem3ExamHeadingTextView;
+    private TextView sem4ExamHeadingTextView;
+    private Button button;
+    private RecyclerView recyclerView;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Module Details");
+    private ModuleDetailViewModel viewModel;
+    private DisqusPostAdapter disqusPostAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar())
+                .setTitle("Module Details");
+
         View root = inflater.inflate(R.layout.fragment_module_detail, container, false);
+        titleTextView = root.findViewById(R.id.module_title);
+        codeTextView = root.findViewById(R.id.module_code);
+        infoTextView = root.findViewById(R.id.module_info);
+        semestersTextView = root.findViewById(R.id.module_semesters);
+        descriptionTextView = root.findViewById(R.id.module_description);
+        prerequisiteTextView = root.findViewById(R.id.module_prerequisite);
+        corequisiteTextView = root.findViewById(R.id.module_corequisite);
+        preclusionTextView = root.findViewById(R.id.module_preclusion);
+        prerequisiteHeaderTextView = root.findViewById(R.id.module_prerequisite_header);
+        corequisiteHeaderTextView = root.findViewById(R.id.module_corequisite_header);
+        preclusionHeaderTextView = root.findViewById(R.id.module_preclusion_header);
+        sem1ExamTextView = root.findViewById(R.id.module_exam_1);
+        sem2ExamTextView = root.findViewById(R.id.module_exam_2);
+        sem3ExamTextView = root.findViewById(R.id.module_exam_3);
+        sem4ExamTextView = root.findViewById(R.id.module_exam_4);
+        sem1ExamHeadingTextView = root.findViewById(R.id.module_exam_1_heading);
+        sem2ExamHeadingTextView = root.findViewById(R.id.module_exam_2_heading);
+        sem3ExamHeadingTextView = root.findViewById(R.id.module_exam_3_heading2);
+        sem4ExamHeadingTextView = root.findViewById(R.id.module_exam_4_heading);
+        button = root.findViewById(R.id.button);
+        recyclerView = root.findViewById(R.id.disqus_posts);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         String moduleCode = ModuleDetailFragmentArgs.fromBundle(getArguments()).getModuleCode();
+
         viewModel = new ViewModelProvider(this,
                 new ModuleDetailViewModelFactory(AcademicYear.getCurrent(), moduleCode))
                 .get(ModuleDetailViewModel.class);
-        observeViewModel(root);
+
+        disqusPostAdapter = new DisqusPostAdapter();
+        DisqusRepository.getInstance().getPosts(moduleCode).observe(getViewLifecycleOwner(),
+                postList -> disqusPostAdapter.setPosts(postList.getPosts()));
+        recyclerView.setAdapter(disqusPostAdapter);
+
+        observeViewModel();
         return root;
     }
 
@@ -96,122 +153,82 @@ public class ModuleDetailFragment extends Fragment {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void observeViewModel(View root) {
-        final TextView titleTextView = root.findViewById(R.id.module_title);
-        final TextView codeTextView = root.findViewById(R.id.module_code);
-        final TextView infoTextView = root.findViewById(R.id.module_info);
-        final TextView semestersTextView = root.findViewById(R.id.module_semesters);
-        final TextView descriptionTextView = root.findViewById(R.id.module_description);
-        final TextView prerequisiteTextView = root.findViewById(R.id.module_prerequisite);
-        final TextView corequisiteTextView = root.findViewById(R.id.module_corequisite);
-        final TextView preclusionTextView = root.findViewById(R.id.module_preclusion);
-        final TextView prerequisiteHeaderTextView = root
-                .findViewById(R.id.module_prerequisite_header);
-        final TextView corequisiteHeaderTextView = root
-                .findViewById(R.id.module_corequisite_header);
-        final TextView preclusionHeaderTextView = root.findViewById(R.id.module_preclusion_header);
-
-        final TextView sem1ExamTextView = root.findViewById(R.id.module_exam_1);
-        final TextView sem2ExamTextView = root.findViewById(R.id.module_exam_2);
-        final TextView sem3ExamTextView = root.findViewById(R.id.module_exam_3);
-        final TextView sem4ExamTextView = root.findViewById(R.id.module_exam_4);
-
-        final TextView sem1ExamHeadingTextView = root.findViewById(R.id.module_exam_1_heading);
-        final TextView sem2ExamHeadingTextView = root.findViewById(R.id.module_exam_2_heading);
-        final TextView sem3ExamHeadingTextView = root.findViewById(R.id.module_exam_3_heading2);
-        final TextView sem4ExamHeadingTextView = root.findViewById(R.id.module_exam_4_heading);
-
-        final Button button = root.findViewById(R.id.button);
-        final RecyclerView recyclerView = root.findViewById(R.id.disqus_posts);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        DisqusPostAdapter adapter = new DisqusPostAdapter();
-        recyclerView.setAdapter(adapter);
-
-        viewModel.getModuleObservable().observe(getViewLifecycleOwner(), m -> {
-            if (m == null) {
+    private void observeViewModel() {
+        viewModel.getModuleObservable().observe(getViewLifecycleOwner(), module -> {
+            if (module == null) {
                 return;
             }
 
-            DisqusRepository.getInstance().getPosts(m.getModuleCode())
-                    .observe(getViewLifecycleOwner(),
-                            postList -> adapter.setPosts(postList.getPosts()));
-
-            codeTextView.setText(m.getModuleCode());
-            titleTextView.setText(m.getTitle());
-            infoTextView.setText(generateInfoText(m.getDepartment(), m.getFaculty(),
-                    m.getCredit()));
-            semestersTextView.setText(generateSemestersText(m.getDetailedSemesters()));
-
-            setTextView(descriptionTextView, m.getDescription());
-
-            setTextView(prerequisiteTextView, generateClickableString(m.getPrerequisite()),
-                    prerequisiteHeaderTextView);
-            prerequisiteTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-
-            setTextView(corequisiteTextView, generateClickableString(m.getCorequisite()),
-                    corequisiteHeaderTextView);
-            corequisiteTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-            setTextView(preclusionTextView, generateClickableString(m.getPreclusion()),
-                    preclusionHeaderTextView);
-            preclusionTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-            TextView[] examViews = new TextView[]{sem1ExamTextView, sem2ExamTextView,
-                    sem3ExamTextView, sem4ExamTextView};
-            TextView[] examHeadingViews = new TextView[]{sem1ExamHeadingTextView,
-                    sem2ExamHeadingTextView, sem3ExamHeadingTextView,
-                    sem4ExamHeadingTextView};
-
-            List<SemesterDetail> semData = m.getDetailedSemesters();
-
-            for (int i = 0; i < semData.size(); i++) {
-                SemesterDetail d = semData.get(i);
-
-                String examDate = d.getExamDate().orElse("No Exam");
-                String examDuration = "";
-
-                if (d.hasExam()) {
-                    examDuration = d.getExamDuration()
-                            .toString().substring(2)
-                            .replaceAll("(\\d[HMS])(?!$)", "$1 ")
-                            .toLowerCase();
-                }
-
-                examViews[d.getSemester().getId() - 1]
-                        .setText(examDate + " " + examDuration);
-            }
-
-            for (int i = 0; i < examHeadingViews.length; i++) {
-                if (examViews[i].getText().length() == 0) {
-                    examViews[i].setVisibility(View.GONE);
-                    examHeadingViews[i].setVisibility(View.GONE);
-                }
-            }
-
-            button.setOnClickListener(view -> {
-                PopupMenu popupMenu = new PopupMenu(getActivity(), button);
-
-                for (SemesterDetail data : semData) {
-                    Semester sem = data.getSemester();
-                    popupMenu.getMenu()
-                            .add(Menu.NONE, sem.getId(), Menu.NONE, sem.toString());
-                }
-
-                popupMenu.setOnMenuItemClickListener(
-                        menuItem -> {
-                            TimetableDataSource.getInstance().insert(new AssignedModule(
-                                    Semester.fromId(menuItem.getItemId()),
-                                    m));
-                            return true;
-                        });
-
-                popupMenu.show();
-            });
+            setTextViews(module);
+            setButton(module);
         });
     }
 
-    private void setTextView(TextView view, CharSequence text, TextView... dependentViews) {
+    private void setButton(ModuleDetail module) {
+        button.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(requireActivity(), button);
+
+            for (SemesterDetail data : module.getDetailedSemesters()) {
+                Semester sem = data.getSemester();
+                popupMenu.getMenu()
+                        .add(Menu.NONE, sem.getId(), Menu.NONE, sem.toString());
+            }
+
+            popupMenu.setOnMenuItemClickListener(
+                    menuItem -> {
+                        TimetableDataSource.getInstance().insert(new AssignedModule(
+                                Semester.fromId(menuItem.getItemId()), module));
+                        return true;
+                    });
+
+            popupMenu.show();
+        });
+    }
+
+    private void setTextViews(ModuleDetail module) {
+        codeTextView.setText(module.getModuleCode());
+        titleTextView.setText(module.getTitle());
+        infoTextView.setText(generateInfoText(module.getDepartment(), module.getFaculty(),
+                module.getCredit()));
+        semestersTextView.setText(generateSemestersText(module.getDetailedSemesters()));
+
+        setOptionalTextView(descriptionTextView, module.getDescription());
+        setOptionalTextView(prerequisiteTextView, generateClickableString(module.getPrerequisite()),
+                prerequisiteHeaderTextView);
+        prerequisiteTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        setOptionalTextView(corequisiteTextView, generateClickableString(module.getCorequisite()),
+                corequisiteHeaderTextView);
+        corequisiteTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        setOptionalTextView(preclusionTextView, generateClickableString(module.getPreclusion()),
+                preclusionHeaderTextView);
+        preclusionTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView[] examViews = new TextView[]{sem1ExamTextView, sem2ExamTextView,
+                sem3ExamTextView, sem4ExamTextView};
+        TextView[] examHeadingViews = new TextView[]{sem1ExamHeadingTextView,
+                sem2ExamHeadingTextView, sem3ExamHeadingTextView,
+                sem4ExamHeadingTextView};
+
+        List<SemesterDetail> semData = module.getDetailedSemesters();
+        List<String> examStrings = Arrays.asList("", "", "", "");
+
+        for (int i = 0; i < semData.size(); i++) {
+            SemesterDetail d = semData.get(i);
+
+            String examDate = d.getExamDate().orElse("No Exam");
+            String examDuration = d.hasExam() ? d.getExamDuration().toString().substring(2)
+                    .replaceAll("(\\d[HMS])(?!$)", "$1 ").toLowerCase()
+                    : "";
+
+            examStrings.set(d.getSemester().getId() - 1, examDate + " " + examDuration);
+        }
+
+        for (int i = 0; i < examHeadingViews.length; i++) {
+            setOptionalTextView(examViews[i], examStrings.get(i), examHeadingViews[i]);
+        }
+    }
+
+    private void setOptionalTextView(TextView view, CharSequence text, TextView... dependentViews) {
         if (text == null || text.length() == 0) {
             view.setVisibility(View.GONE);
 
