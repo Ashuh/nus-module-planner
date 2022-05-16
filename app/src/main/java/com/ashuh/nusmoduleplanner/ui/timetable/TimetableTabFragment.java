820c284cc3c27c3fa4850bb17fd089d75f741131
Ashuh <ashuh223@gmail.com>
2022-05-16 21:06:34 +0800

    Implement Observer in TimetableTabFragment

--- app/src/main/java/com/ashuh/nusmoduleplanner/ui/timetable/TimetableTabFragment.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/ui/timetable/TimetableTabFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,16 +20,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ashuh.nusmoduleplanner.MainActivity;
 import com.ashuh.nusmoduleplanner.R;
 import com.ashuh.nusmoduleplanner.data.model.nusmods.module.semesterdatum.SemesterType;
+import com.ashuh.nusmoduleplanner.data.model.timetable.AssignedModule;
 import com.ashuh.nusmoduleplanner.data.model.timetable.TimetableEvent;
 import com.ashuh.nusmoduleplanner.data.source.timetable.TimetableDAO;
 import com.ashuh.nusmoduleplanner.data.source.timetable.TimetableDataSource;
 import com.ashuh.nusmoduleplanner.data.source.timetable.TimetableDatabase;
 
+import java.util.List;
+
 import me.jlurena.revolvingweekview.WeekView;
 import me.jlurena.revolvingweekview.WeekViewEvent;
 
-public class TimetableTabFragment extends Fragment implements WeekView.EventClickListener {
+public class TimetableTabFragment extends Fragment implements WeekView.EventClickListener,
+        Observer<List<AssignedModule>> {
+
     public static final String ARG_SEMESTER = "semester";
+
+    private TimetableView timetableView;
     private SemesterType semType;
     private TimetableViewModel viewModel;
     private TimetableEntryAdapter adapter;
@@ -63,17 +71,10 @@ public class TimetableTabFragment extends Fragment implements WeekView.EventClic
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        TimetableView timetableView = view.findViewById(R.id.revolving_weekview);
+        timetableView = view.findViewById(R.id.revolving_weekview);
         timetableView.setSemType(semType);
-
-        viewModel.getTimetableEntriesObservable().observe(getViewLifecycleOwner(), entries -> {
-            if (entries == null) {
-                return;
-            }
-            adapter.setAssignedModules(entries);
-            timetableView.setAssignedModules(entries);
-            timetableView.setOnEventClickListener(this);
-        });
+        timetableView.setOnEventClickListener(this);
+        viewModel.getTimetableEntriesObservable().observe(getViewLifecycleOwner(), this);
     }
 
     @Override
@@ -88,5 +89,15 @@ public class TimetableTabFragment extends Fragment implements WeekView.EventClic
         FragmentManager fragmentManager
                 = ((MainActivity) requireContext()).getSupportFragmentManager();
         fragment.show(fragmentManager, "lessons");
+    }
+
+    @Override
+    public void onChanged(List<AssignedModule> assignedModules) {
+        if (assignedModules == null) {
+            return;
+        }
+
+        adapter.setAssignedModules(assignedModules);
+        timetableView.setAssignedModules(assignedModules);
     }
 }
