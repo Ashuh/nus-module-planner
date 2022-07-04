693bb5bdc380b9342476e1f54093713e8aaae881
Ashuh <ashuh223@gmail.com>
2022-07-05 05:39:27 +0800

    Migrate to androidx SearchView

--- app/src/main/java/com/ashuh/nusmoduleplanner/modulelist/presentation/ModuleListFragment.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/modulelist/presentation/ModuleListFragment.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
