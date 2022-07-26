947099fbb8780312365d3ffcb3c14f6eb89a85c0
Ashuh <ashuh223@gmail.com>
2022-07-27 00:46:30 +0800

    Remove unused import

--- app/src/main/java/com/ashuh/nusmoduleplanner/timetable/presentation/ColorSelectDialogFragment.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/timetable/presentation/ColorSelectDialogFragment.java
@@ -1,7 +1,5 @@
 package com.ashuh.nusmoduleplanner.timetable.presentation;
 
-import static java.util.Objects.requireNonNull;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
