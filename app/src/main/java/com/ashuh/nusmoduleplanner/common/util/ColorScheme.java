95bbff7c6d1de40eeba0a29bb588d8c94a368a07
Ashuh <ashuh223@gmail.com>
2022-07-27 22:02:25 +0800

    Add eighties color scheme

--- app/src/main/java/com/ashuh/nusmoduleplanner/common/util/ColorScheme.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/common/util/ColorScheme.java
@@ -10,6 +10,15 @@ import java.util.List;
 
 public enum ColorScheme {
 
+    EIGHTIES(Colors.EIGHTIES_RED,
+            Colors.EIGHTIES_ORANGE,
+            Colors.EIGHTIES_YELLOW,
+            Colors.EIGHTIES_GREEN,
+            Colors.EIGHTIES_DARK_BLUE,
+            Colors.EIGHTIES_LIGHT_BLUE,
+            Colors.EIGHTIES_PURPLE,
+            Colors.EIGHTIES_PINK),
+
     GOOGLE(Colors.GOOGLE_RED,
             Colors.GOOGLE_ORANGE,
             Colors.GOOGLE_YELLOW,
@@ -54,5 +63,14 @@ public enum ColorScheme {
         private static final Color GOOGLE_LIGHT_BLUE = Color.valueOf(0xFF79A4F9);
         private static final Color GOOGLE_PURPLE = Color.valueOf(0xFFA36AC7);
         private static final Color GOOGLE_PINK = Color.valueOf(0xFFEC9998);
+
+        private static final Color EIGHTIES_RED = Color.valueOf(0xFFF2777A);
+        private static final Color EIGHTIES_ORANGE = Color.valueOf(0xFFf99157);
+        private static final Color EIGHTIES_YELLOW = Color.valueOf(0xFFFFCC66);
+        private static final Color EIGHTIES_GREEN = Color.valueOf(0xFF99CC99);
+        private static final Color EIGHTIES_DARK_BLUE = Color.valueOf(0xFF66CCCC);
+        private static final Color EIGHTIES_LIGHT_BLUE = Color.valueOf(0xFF6699CC);
+        private static final Color EIGHTIES_PURPLE = Color.valueOf(0xFFCC99CC);
+        private static final Color EIGHTIES_PINK = Color.valueOf(0xFFD27B53);
     }
 }
