2c8952274ac6069be4353e62b250514370f1edc3
Ashuh <ashuh223@gmail.com>
2022-10-14 12:09:36 +0800

    Add new color scheme settings page

--- app/src/main/java/com/ashuh/nusmoduleplanner/settings/SettingsFragment.java
+++ app/src/main/java/com/ashuh/nusmoduleplanner/settings/SettingsFragment.java
@@ -3,47 +3,27 @@ package com.ashuh.nusmoduleplanner.settings;
 import static com.ashuh.nusmoduleplanner.common.data.preferences.SharedPreferencesManager.PREFERENCE_FILE_KEY;
 
 import android.os.Bundle;
-import android.view.Menu;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.preference.ListPreference;
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.navigation.Navigation;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 
 import com.ashuh.nusmoduleplanner.R;
-import com.ashuh.nusmoduleplanner.common.util.ColorScheme;
 
-import java.util.Arrays;
-
-public class SettingsFragment extends PreferenceFragmentCompat {
-    @Override
-    public void onPrepareOptionsMenu(@NonNull Menu menu) {
-        menu.clear();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
-
+public class SettingsFragment extends NoMenuPreferenceFragment {
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         PreferenceManager manager = getPreferenceManager();
         manager.setSharedPreferencesName(PREFERENCE_FILE_KEY);
         setPreferencesFromResource(R.xml.preferences, rootKey);
-        ListPreference colorSchemePreference = findPreference("color_scheme");
-        assert colorSchemePreference != null;
 
-        ColorScheme[] colorSchemes = ColorScheme.values();
-        CharSequence[] entries = Arrays.stream(colorSchemes)
-                .map(ColorScheme::toString)
-                .toArray(CharSequence[]::new);
-        CharSequence[] entryValues = Arrays.stream(colorSchemes)
-                .map(ColorScheme::name)
-                .toArray(CharSequence[]::new);
-        colorSchemePreference.setEntries(entries);
-        colorSchemePreference.setEntryValues(entryValues);
+        Preference colorSchemePreference = findPreference("color_scheme");
+        assert colorSchemePreference != null;
+        colorSchemePreference.setOnPreferenceClickListener(preference -> {
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+                    .navigate(R.id.action_nav_settings_main_to_colorSchemeSettingsFragment);
+            return true;
+        });
     }
 }
